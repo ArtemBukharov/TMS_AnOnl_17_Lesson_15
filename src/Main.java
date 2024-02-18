40f19c20cb9631cd3fd53dd54b4a214cf1a08444
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-02-18 22:21:10 +0300

    Sinchronized

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runni());
+        thread1.setName("one");
+        Thread thread2 = new Thread(new Runni());
+        thread2.setName("two");
+        Thread thread3 = new Thread(new Runni());
+        thread3.setName("three");
+        try {
+            thread3.start();
+            thread3.join();
+            thread2.start();
+            thread2.join();
+            thread1.start();
+            thread1.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("main potok");
+    }
+}
+
+class Runni implements Runnable {
+    public static int count=0;
+    public static Object lock=new Object();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            synchronized (lock){
+                count++;
+            }
+            System.out.println("В потоке :" + Thread.currentThread().getName());
+        }
+        System.out.println(count);
+    }
+}
